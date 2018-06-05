014f05a5f05368b49200903fc406366751a4e3b8
Priyashantha <priyashantha@silverstripers.com>
2018-06-05 18:03:12 +0530

    keep the previous code as it is and return in if condition

--- code/model/Translatable.php
+++ code/model/Translatable.php
@@ -1731,20 +1731,20 @@ class Translatable extends DataExtension implements PermissionProvider
             && count($controller->getRequest()->getVars())
         ) {
             $tags .= '<link rel="canonical" href="'.$controller->AbsoluteLink().'" />';
-        } else {
-            $template = '<link rel="alternate" type="text/html" title="%s" hreflang="%s" href="%s" />' . "\n";
-            $translations = $this->owner->getTranslations();
-            if($translations->count()) {
-                $translations = $translations->toArray();
-                $translations[] = $this->owner;
+            return;
+        }
+        $template = '<link rel="alternate" type="text/html" title="%s" hreflang="%s" href="%s" />' . "\n";
+        $translations = $this->owner->getTranslations();
+        if($translations->count()) {
+            $translations = $translations->toArray();
+            $translations[] = $this->owner;
 
-                foreach($translations as $translation) {
-                    $tags .= sprintf($template,
-                        Convert::raw2xml($translation->Title),
-                        i18n::convert_rfc1766($translation->Locale),
-                        $translation->AbsoluteLink()
-                    );
-                }
+            foreach($translations as $translation) {
+                $tags .= sprintf($template,
+                    Convert::raw2xml($translation->Title),
+                    i18n::convert_rfc1766($translation->Locale),
+                    $translation->AbsoluteLink()
+                );
             }
         }
     }
